9ff492fc945f4d9dde3d6a0a42904aaa1a905704
hamdi <javs_shao@163.com>
2018-12-25 15:09:59 +0800

    编写Spider_提取信息

--- MySpider/MySpider/spiders/itcast.py
+++ MySpider/MySpider/spiders/itcast.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from MySpider.items import ItcastItem
 
 
 class ItcastSpider(scrapy.Spider):
@@ -8,5 +9,26 @@ class ItcastSpider(scrapy.Spider):
     start_urls = ['http://www.itcast.cn/channel/teacher.shtml']
 
     def parse(self, response):
-        filename = "teacher.html"
-        open(filename, 'wb').write(response.body)
+        # filename = "teacher.html"
+        # open(filename, 'wb').write(response.body)
+
+        # 存放职工信息的集合
+        items = []
+
+        for each in response.xpath('//div[@class="li_txt"]'):
+            # 将我们得到的数据封装到一个 'ItcastItem 对象'
+            item = ItcastItem()
+            # extract()方法返回的都是unicode字符串
+            name = each.xpath('h3/text()').extract()
+            title = each.xpath('h4/text()').extract()
+            info = each.xpath('p/text()').extract()
+
+            # xpath返回的是包含一个元素的列表
+            item['name'] = name[0]
+            item['title'] = title[0]
+            item['info'] = info[0]
+
+            items.append(item)
+
+        # 直接返回数据
+        return items
